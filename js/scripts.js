027367718d71c2e3f5e0d839eaf3e803b3a3e1ea
Kennedy777 <46945500+Kennedy777@users.noreply.github.com>
2019-04-01 06:44:43 -0700

    revised js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -30,95 +30,22 @@ $('#quest4 #submit').click() {
   }
 });
 
-//  let isAdult = false;
-  var favColor;
-  $('#colorPick').change(function(){
-    favColor = $('#colorPick').val();
-  });
-
   $("#quiz").submit(function(event) {
     event.preventDefault();
-    console.log("submit being called");
-    var age = parseInt($("#age").val());
-    console.log("Age is " + age);
-    var gender = $("#gender").val();
-    console.log("Gender is "+gender);
-    var orientation = $("select#orientation").val();
-    isAdult=ageCheck(age);
-    if(!isAdult) {
-      console.log('Age is too Young!');
-      //$('#tooYoung').show();
-      return;
-    }
-    genderPath(gender, orientation);
-    hobbyPath(favColor);
+     
+    var whichEnd = $('input[name=endType]:checked').val();
+    var where = $('input[name=location]:checked').val();
+    var langType = $('input[progType]:checked').val();
+    var company = $('input[bizType]:checked').val();
 
+    if(whichEnd === "front") {
+      $(".result").text("Take the UX/UI and design track. Hope you're in Portland, Oregon!");
+    } else if (whichEnd === "back" || money === "mix") {
+      if(company === "corp") {
+        $(".result").text("C# or Java would be great for you!");
+      } else { 
+            $(".result").text("Ruby on Rails would be a great starting point!");
+        }
+      }
 
-
-
-  function ageCheck(myAge) {
-    if(myAge>=18) {
-      return true;
-    }else{
-      return false;
-    }
-  }
-  function genderPath(myGender, orientation) {
-    switch (myGender) {
-      case 'male':
-        console.log("case is male");
-        maleAlg(orientation);
-        break;
-      case 'female':
-        console.log("case is female");
-        femaleAlg(orientation);
-        break;
-      case 'nonbinary':
-        console.log("case is nonbinary");
-        nonbinaryAlg(orientation);
-        break;
-
-
-    }
-  }
-  function maleAlg(ori){
-    switch (ori) {
-      case 'straight':
-        $("#showAnswer > div:not('.straightM')").hide();
-        $('#showAnswer').find('.straightM').show();
-        break;
-      case 'queer':
-        $("#showAnswer > div:not('.queerM')").hide();
-        $('#showAnswer').find('.queerM').show();
-        break;
-    }
-  }
-  function femaleAlg(ori){
-    switch (ori){
-      case 'straight':
-        $("#showAnswer > div:not('.straightF')").hide();
-        $('#showAnswer').find('.straightF').show();
-        break;
-      case 'queer':
-        $("#showAnswer > div:not('.queerF')").hide();
-        $('#showAnswer').find('.queerF').show();
-        break;
-    }
-  }
-
-  function nonbinaryAlg(ori){
-    switch (ori){
-      case 'straight':
-        $("#showAnswer > div:not('.straightNB')").hide();
-        $('#showAnswer').find('.straightNB').show();
-        break;
-      case 'queer':
-        $("#showAnswer > div:not('.queerNB')").hide();
-        $('#showAnswer').find('.queerNB').show();
-        break;
-    }
-  }
-  function hobbyPath(color){
-    $('#showAnswer').find('h1').css('color', color)
-  }
 });
