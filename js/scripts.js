e27cdeeecbf394714eb4794afd158bf538e5e681
Kennedy777 <46945500+Kennedy777@users.noreply.github.com>
2019-04-08 04:47:04 -0700

    change from drop-down tro radio boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
 $(document).ready(function(){
   $("#selectionForm").submit(function(event){
+    event.preventDefault();
+    var whichEnd = $("input:radio[name=focusArea]:checked").val();
+    var where = $("input:radio[name=location]:checked").val();
+    var area = $("input:radio[name=interest]:checked").val();
+    var company = $("input:radio[name=bizType]:checked").val();
+    var priorities = $("input:radio[name=whatIsImpt]:checked").val();
 
-    var whichEnd = $('select#focus').val();
-    var where = $('select#campus').val();
-    var area = $('select#interest').val();
-    var company = $('select#org').val();
-    var priorities = $('select#values').val();
-
-    if(whichEnd === "front") {
-      langRec="Take the UX/UI and design track. Hope you're in Portland, Oregon!");
-    } else if (whichEnd === "back" || "mix" && where ==="seattle") {
-        langRec="C# or Java would be great for you!");
-      } else {
-          langRec="Ruby on Rails would be a great starting point!");
-        }
-
-    if(company==="corp") {
-      langRec = "Java or C# would be a good choice for you."
-    } else if (company==="govt") {
-      langRec="PHP or Java would be a great option."
-    }
-  $("langRec").text(langRec);
-  $(".output").text();
-    
-  event.preventDefault();
+//business logic//
+  if(whichEnd === "front") {
+  $("#frontResult").show();
+   }
+  if(whichEnd != "front" && company==="corp"){
+  $("#corJavaResult").show();
+  }
+ if (company !="startUp"){
+ $("#rubyResult").fadeIn("slow");
+ }
+  if(whichEnd != "front" && priorities==="foosball") {
+  $("#rubyResult").show();
+   }else{
+    $("#corJavaResult").show();
+   }
   });
 });
