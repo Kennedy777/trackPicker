884ac028e9d1017ad6fe45ea5b470940a863619c
Kennedy777 <reach.marguerite@gmail.com>
2019-04-18 08:18:22 -0700

    fixed file name problem with js

--- js/scripts.js.js
+++ js/scripts.js.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
-  $("form#survey").submit(function(event){
+$(document).ready(function(){
+  $("#selectionForm").submit(function(event){
     event.preventDefault();
-    var company = parseInt($("select#company").val());
+    var bizType = parseInt($("select#bizType").val());
     var interests = parseInt($("select#interests").val());
     var cuisine = parseInt($("select#cuisine").val());
     var where = parseInt($("select#where").val());
     var pet = parseInt($("select#pet").val());
-    var total = company + interests + cuisine + people + where;
+    var total = bizType + interests + cuisine + people + where;
 
     //business logic//
     if (total <= 7) {
-      $(".jumbotron").show();
+      $("#result").show();
       $(".langCSS").fadeIn();
       } else if (total > 8 && total <= 12) {
-      $(".jumbotron").show();
+      $("#result").show();
       $(".rubyResult").fadeIn();
       } else if (total > 13 && total <= 15) {
-      $(".jumbotron").show();
+      $("#result").show();
       $(".cResult").fadeIn();
-      }
-
-    });
+    }
+  });
 });
